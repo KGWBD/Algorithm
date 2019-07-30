723a3f5dd7b951a167ab4ab48afbedabc6d1db5a
김건우 <kin0984@naver.com>
2019-07-30 17:35:11 +0900

    거듭 제곱(SwExper1217)
    두개의 숫자가 주어질 때재귀호출로 거듭제곱을 구하는 문제

---
+++ SwExpert/src/Problem1217.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Problem1217 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		sc.nextInt();
+		for(int t =1; t <=10;t++) {	
+		int base = sc.nextInt();
+		int exp = sc.nextInt();
+		int result = doPow(base,exp);
+		System.out.format("#%d %d", t,result);
+		}
+	}
+	
+	static int doPow(int base, int exp) {
+		if(exp==0) return 1;
+		 else if(exp==1) return base;
+		
+		int num = doPow(base,exp/2);
+		int result = num * num;
+		if(exp%2!=0) {
+			result *= base;
+		}
+		
+		
+		return result; 
+	}
+}
