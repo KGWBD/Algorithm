226a2ea48af9ddc6811a2e3deb3bcf60d94049c5
김건우 <kin0984@naver.com>
2019-08-16 23:16:17 +0900

    변경 파일

--- SwExpert/src/Problem1289.java
+++ SwExpert/src/Problem1289.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solution {
+public class Problem1289 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
--- SwExpert/src/Solution123.java
+++ SwExpert/src/Solution123.java
@@ -1,56 +1,23 @@
-import java.util.Scanner;
-
 public class Solution123 {
-	static boolean[] visit;
-	static int max = -1;
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int testCase = sc.nextInt();
-		for (int t = 1; t <= testCase; t++) {
-
-			int N = sc.nextInt();
-			int M = sc.nextInt();
-			int y, x;
-			int[][] arr = new int[N + 1][N + 1];
-			visit = new boolean[N + 1];
-			for (int i = 1; i < visit.length; i++) {
-				visit[i] = true;
-			}
-
-			for (int m = 0; m < M; m++) {
-				y = sc.nextInt();
-				x = sc.nextInt();
-				arr[y][x] = 1;
-				arr[x][y] = 1;
-			}
-			boolean[] visited = new boolean[N + 1];
-			for (int i = 1; i <= N; i++) {
-				for (int j = 1; j <= N; j++) {
-					if (arr[i][j] == 1)
-						dfs(arr, i, visited, 1);
+		String s = "Rhs0266";
+		char[] chars = s.toCharArray();
+		for(int k=0;k<chars.length;k++) {
+			if(k==0) {
+				if(chars[k]-'A'<0 || chars[k]-'A'>25) {
+					System.out.println("1 "+k);
+					continue;
+				}
+			} else {
+				if(chars[k]-'A'>=0 && chars[k]-'A'<=25) {
+					System.out.println("2 "+k);
+					continue;
+				} else if(chars[k]-'0' >= 0 && chars[k] -'0' <9) {
+					contin
 				}
 			}
-			System.out.format("#%d ",t);
-			System.out.println(max == -1 ? 1 : max);
-			
 		}
-	}
-
-	private static void dfs(int[][] arr, int node, boolean[] visited, int cnt) {
-		if (visit[node]) {
-			max = Math.max(max, cnt);
-			return;
-		}
-		
-		visited[node] = true;
-		for (int i = 0; i < arr.length; i++) {
-			if (!visited[i] && arr[node][i] == 1) {
-				dfs(arr, i, visited, cnt + 1);
-			}
-		}
-		
-
 	}
 
 }
