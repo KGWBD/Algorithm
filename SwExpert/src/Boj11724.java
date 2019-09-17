20e33a790234b9ab3907a75401cdf72f09aaaed9
김건우 <kin0984@naver.com>
2019-09-17 17:44:10 +0900

    연결요소의개수(백준11724)

---
+++ SwExpert/src/Boj11724.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj11724 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static int[] p;
+	static int N,M;
+	
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		p = new int[N+1];
+		makeSet();
+		for(int i = 0 ; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int n1 = Integer.parseInt(st.nextToken());
+			int n2 = Integer.parseInt(st.nextToken());
+			union(n1,n2);
+		}
+		int cnt = 0;
+		for(int i = 1; i <=N;i++) {
+			if(p[i]==i) {
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
+	}
+	
+	static void makeSet() {
+		for(int i = 1; i <=N;i++) {
+			p[i] = i;
+		}
+	}
+	
+	static int findSet(int x) {
+		if(p[x]==x)return x;
+		int idx = findSet(p[x]);
+		return idx;
+	}
+	static void union(int x,int y) {
+		int px = findSet(x);
+		int py = findSet(y);
+		if(px!=py)p[py]=px;
+	}
+}
