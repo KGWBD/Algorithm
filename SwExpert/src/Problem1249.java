46a55ff5d7c229f7ba621397a7e720bab51d8a19
김건우 <kin0984@naver.com>
2019-10-05 20:20:17 +0900

    보급로(SwExpert)

--- SwExpert/src/Problem1249.java
+++ SwExpert/src/Problem1249.java
@@ -1,91 +1,90 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
-
-public class Solution {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	static int[][] map;
-	static Node[][] p;
-	static int N, L, R;
-	static ArrayList<Node> list;
-	static int[] dx = { 0, 0, -1, 1 };
-	static int[] dy = { -1, 1, 0, 0 };
-
-	public static void main(String[] args) throws IOException {
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		L = Integer.parseInt(st.nextToken());
-		R = Integer.parseInt(st.nextToken());
-
-		map = new int[N][N];
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		p = new Node[N][N];
-		for (int i = 0; i < N; i++) { // 경로 다 염
-			for (int j = 0; j < N; j++) {
-				if (p[i][j].x == i && p[i][j].y == j) {
-					list = new ArrayList<>();
-					checkPopulation(i, j, map[i][j]);
-					System.out.println("=11=1=1=");
+	import java.io.BufferedReader;
+	import java.io.BufferedWriter;
+	import java.io.IOException;
+	import java.io.InputStreamReader;
+	import java.io.OutputStreamWriter;
+	import java.util.LinkedList;
+	import java.util.Queue;
+	
+	public class Problem1249 {
+		static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		static char[][] map;
+		static int[][] timeArr;
+		static int[][] valueArr;
+		static int[] dx = {0,0,-1,1};
+		static int[] dy = {-1,1,0,0};
+		static int N;
+	    public static void main(String[] args) throws NumberFormatException, IOException {
+	    	int T = Integer.parseInt(br.readLine());
+	//    	int T = 1;
+	    	
+	    	for(int t=1 ; t <= T; t++) {
+	    		N = Integer.parseInt(br.readLine());
+	    		map = new char[N][N];
+	    		timeArr = new int[N][N];
+	    		valueArr = new int[N][N];
+	    		
+	    		for(int i = 0 ; i < N ; i++) {
+	    			map[i] = br.readLine().toCharArray();
+	    		}
+	    		for(int i = 0 ; i < N; i++) {
+	    			for(int j = 0 ; j < N; j++) {
+	    				timeArr[i][j] = map[i][j] - '0';
+	    			}
+	    		}
+	    		
+	    		searchShortLenght();
+	    		
+	    		bw.write("#"+t+" " );
+	    		bw.write(valueArr[N-1][N-1]-1+"\n");
+	    	}//testCase
+	    	bw.close();
+	    	
+	    }
+		private static void searchShortLenght() {
+			
+			Queue<Pair> q = new LinkedList<Pair>();
+			q.offer(new Pair(0,0));
+			valueArr[0][0] = 1;
+			int ny,nx,nextVal;
+			while(!q.isEmpty()) {
+				Pair p = q.poll();
+				
+				for(int i = 0 ; i < 4; i++) {
+					
+					ny = p.y + dy[i];
+					nx = p.x + dx[i];
+					
+					if(!isRange(ny,nx))continue;
+					
+					nextVal = timeArr[ny][nx] + valueArr[p.y][p.x];
+					
+					if(valueArr[ny][nx]==0 || nextVal < valueArr[ny][nx]) {
+						q.offer(new Pair(ny,nx));
+						valueArr[ny][nx] = nextVal;
+					}
+					
+					
 				}
 			}
+			
+			
 		}
-
-		System.out.println(map[0][0]);
-
-	}
-
-	private static void checkPopulation(int y, int x, int popul) {
-		// 4방향으로 탐색하면서 연합
-
-		int ny, nx;
-		for (int i = 0; i < 4; i++) {
-			ny = y + dy[i];
-			nx = y + dx[i];
-
-			if (!isRange(ny, nx))
-				continue;
-
-			if (Math.abs(popul - map[ny][nx]) >= L && Math.abs(popul - map[ny][nx]) <= R) {
-				checkPopulation(ny, nx, map[ny][nx]);
+		
+		static boolean isRange(int ny,int nx) {
+			if(ny == N|| ny<0 ||nx==N||nx<0)return false;
+			return true;
+		}
+		static class Pair{
+			int y,x;
+			Pair(int y, int x){
+				this.y = y;
+				this.x = x;
 			}
 		}
-
-		int sum = 0;
-		for (int i = 0; i < list.size(); i++) {
-			sum += list.get(i).popul;
-		}
-		for (int i = 0; i < list.size(); i++) {
-			map[list.get(i).y][list.get(i).x] = sum / list.size();
-		}
-
+	 
+	 
+	 
+	 
 	}
-
-	private static boolean isRange(int ny, int nx) {
-		if (ny < 0 || nx < 0 || ny == N || nx == N)
-			return false;
-		return true;
-	}
-
-	static class Node {
-		int y, x, popul;
-
-		Node(int y, int x) {
-			this.y = y;
-			this.x = x;
-		}
-
-		Node(int y, int x, int popul) {
-			this(y, x);
-			this.popul = popul;
-		}
-
-	}
-}
