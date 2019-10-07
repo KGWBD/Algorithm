261f8640fc29868d599e97a236e8fbce2713d300
김건우 <kin0984@naver.com>
2019-10-07 23:45:55 +0900

    최소비용구하기

--- SwExpert/src/Boj1916.java
+++ SwExpert/src/Boj1916.java
@@ -13,59 +13,90 @@ public class Boj1916 {
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringTokenizer st;
 	static ArrayList<Boj1916.Node>[] list;
-	static boolean[] visited;
+	static boolean[][] visited;
 	static int[] dist;
 	static final int INF = Integer.MAX_VALUE;
 	static int nV, nE;
-	static ArrayList<Node> ansList;
-	static int end;
+	static ArrayList<Integer> ans;
+	static StringBuilder sb;
+	static int start,end;
+	static boolean flag;
 	public static void main(String[] args) throws IOException {
 		nV = Integer.parseInt(br.readLine());
 		nE = Integer.parseInt(br.readLine());
-		visited = new boolean[nV+1];
+		visited = new boolean[nV+1][nV+1];
+		ans = new ArrayList<Integer>();
 		list = new ArrayList[nV+1];
 		for(int i = 1 ; i <=nV; i++) {
 			list[i] = new ArrayList<Boj1916.Node>();
 		}
 		dist = new int[nV+1];
 		Arrays.fill(dist, INF);
-		ansList = new ArrayList<Node>();
+		
 		for(int i = 0; i<nE; i++) {
 			st = new StringTokenizer(br.readLine());
 			int now = Integer.parseInt(st.nextToken());
 			int next = Integer.parseInt(st.nextToken());
 			int cost = Integer.parseInt(st.nextToken());
-			list[now].add(new Node(next,cost, "",0));
+			list[now].add(new Node(next,cost));
 		}
 		st = new StringTokenizer(br.readLine());
-		int start = Integer.parseInt(st.nextToken());
+		start = Integer.parseInt(st.nextToken());
 		end = Integer.parseInt(st.nextToken());
 		
 		dijstra(start);
-		System.out.println(dist[end]);
-		System.out.println(ansList.get(ansList.size()-1).cnt);
-		System.out.println(ansList.get(ansList.size()-1).path);
-//		System.out.println(Arrays.toString(dist));
-	}
 		
+		/*
+		 * for(int i = 1; i < nV+1;i++) { for(int j = 1 ; j < nV+1;j++) {
+		 * System.out.print(visited[i][j]+ " "); } System.out.println(); }
+		 */
+		
+		ans.add(start);
+		searchRoute(start);
+		System.out.println(dist[end]);
+		
+		System.out.println(ans.size());
+		sb = new StringBuilder();
+		for(int n:ans) {
+			sb.append(n + " ");
+		}
+		System.out.println(sb.toString().trim());
+	}
+	private static void searchRoute(int n) {
+		if(n == end) {
+			flag = true;
+			return;
+		}
+		
+		for(int i = 1; i < nV+1; i++) {
+			if(visited[n][i]) {
+//				System.out.println("start ::" + n + "   end:: " + i);
+				Integer a = new Integer(i);
+				ans.add(a);
+//				for(int j:ans) {
+//					System.out.println(j);
+//				}
+				searchRoute(i);
+				if(flag)break;
+				ans.remove(a);
+				
+			}
+		}
+	}
 	private static void dijstra(int start) {
 		PriorityQueue<Node> pq = new PriorityQueue<Node>();
 		dist[start] = 0;
-		pq.offer(new Node(start,dist[start],"" + start,1));
+		pq.offer(new Node(start,dist[start]));
 		Node node;
 		while(!pq.isEmpty()) {
 			node = pq.poll();
-			
 			for(int i = 0; i< list[node.index].size();i++) {
 				Node nextNode = list[node.index].get(i);
 				
-				if(dist[nextNode.index] > dist[node.index] + nextNode.cost) {
+				if(dist[nextNode.index] >= dist[node.index] + nextNode.cost) {
 					dist[nextNode.index] = dist[node.index] + nextNode.cost;
-					Node newNode = new Node(nextNode.index,dist[nextNode.index],node.path+" "+nextNode.index,node.cnt+1);
-					pq.offer(new Node(nextNode.index,dist[nextNode.index],node.path+" "+nextNode.index,node.cnt+1));
-					if(nextNode.index==end) {
-						ansList.add(newNode);
-					}
+					pq.offer(new Node(nextNode.index,dist[nextNode.index]));
+					visited[node.index][nextNode.index] = true;
 				}
 				
 			}
@@ -84,13 +115,9 @@ public class Boj1916 {
 	static class Node implements Comparable<Node>{
 		int index;
 		int cost;
-		String path;
-		int cnt;
-		Node(int index, int cost,String path,int cnt){
+		Node(int index, int cost){
 			this.index = index;
 			this.cost = cost;
-			this.path = path;
-			this.cnt = cnt;
 		}
 		@Override
 		public int compareTo(Node o) {
@@ -98,8 +125,7 @@ public class Boj1916 {
 		}
 		@Override
 		public String toString() {
-			return "Node [index=" + index + ", cost=" + cost + ", path=" + path + ", cnt=" + cnt + "]";
+			return "Node [index=" + index + ", cost=" + cost + "]";
 		}
-		
 	}
 }
