98638ec5d373a7a8f26e34e01c49e410a1ca7113
김건우 <kin0984@naver.com>
2019-10-13 17:44:25 +0900

    파일 수정

--- SwExpert/src/Solution.java
+++ SwExpert/src/Solution.java
@@ -1,86 +1,85 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
+ 
 public class Solution {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	static StringTokenizer st;
-	static int[][] map;
-	static int[][] dp;
-	static int[] dx = { 0, 0, -1, 1 };
-	static int[] dy = { -1, 1, 0, 0 };
-	static int N;
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
-//		int T = Integer.parseInt(br.readLine());
-		int T = 1;
-
-		for (int t = 1; t <= T; t++) {
-			N = Integer.parseInt(br.readLine());
-			map = new int[N][N];
-			dp = new int[N][N];
-			for (int i = 0; i < N; i++) {
-				Arrays.fill(dp[i], -1);
-			}
-
-			for (int i = 0; i < N; i++) {
-				st = new StringTokenizer(br.readLine());
-				for (int j = 0; j < N; j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());
-				}
-			}
-
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					dfs(i, j, 0);
-				}
-			}
-			int maxY = 0;
-			int maxX = 0;
-			int max = -1;
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					if (max < dp[i][j]) {
-						max = dp[i][j];
-						maxY = i;
-						maxX = j;
-					}
-				}
-			}
-			bw.write("#" + t + " ");
-			bw.write(maxY + " " + maxX + " \n");
-		}
-		bw.close();
-	}
-
-	private static int dfs(int y, int x, int cnt) {
-
-		int ny, nx;
-		int temp = 0;
-		for (int i = 0; i < 4; i++) {
-			ny = y + dy[i];
-			nx = x + dx[i];
-
-			if (!isRange(ny, nx))
-				continue;
-
-			if (map[y][x] + 1 == map[ny][nx]) {
-				dfs(y, x, cnt + 1);
-			}
-		}
-
-		return cnt;
-
-	}
-
-	private static boolean isRange(int ny, int nx) {
-		if (ny < 0 || ny == N || nx < 0 || nx == N)
-			return false;
-		return true;
-	}
+    public static StringTokenizer stk;
+    public static StringBuilder sb = new StringBuilder();
+    public static int[] dx = {-1, 0, 0, 1};
+    public static int[] dy = {0, -1, 1, 0};
+    public static int n, eating = 0;
+ 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        int[][] map = new int[n][n];
+        Queue<Shark> q = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            stk = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(stk.nextToken());
+                if (map[i][j] == 9) {
+                    q.add(new Shark(i, j, 2, 0));
+                    map[i][j] = 0;
+                }
+            }
+        }
+ 
+        int ans = 0;
+        boolean[][] visited = new boolean[n][n];
+        while (!q.isEmpty()) {
+            Shark shark = q.poll();
+            if (!visited[shark.x][shark.y]) {
+                visited[shark.x][shark.y] = true;
+                if (map[shark.x][shark.y] != 0 && map[shark.x][shark.y] < shark.power) {  //�� ���̸� ã����
+                    Shark curr = shark;
+                    //�Ÿ� > ���� > ���� ������ �Ա� ���� ���� Queue Ž��
+                    while (!q.isEmpty()) {
+                        shark = q.poll();
+                        if (map[shark.x][shark.y] != 0 && map[shark.x][shark.y] < shark.power) {
+                            if (curr.cost == shark.cost) {
+                                if (curr.x > shark.x) {
+                                    curr = shark;
+                                } else if (curr.x == shark.x && curr.y > shark.y) {
+                                    curr = shark;
+                                }
+                            } else if (curr.cost > shark.cost) {
+                                curr = shark;
+                            }
+                        }
+                    }
+                    map[curr.x][curr.y] = 0;
+                    eating++;
+                    ans += curr.cost;
+                    int flag = 0;
+                    if (eating == curr.power) {        //�� ������ �� ������
+                        flag = 1;
+                        eating = 0;
+                    }
+                    q.clear();
+                    q.add(new Shark(curr.x, curr.y, curr.power + flag, 0));
+                    visited = new boolean[n][n];
+                    continue;
+                }
+                for (int i = 0; i < 4; i++) {
+                    int nx = shark.x + dx[i];
+                    int ny = shark.y + dy[i];
+                    if (nx >= 0 && nx < n && ny >= 0 && ny < n && map[nx][ny] <= shark.power && !visited[nx][ny]) {
+                        q.add(new Shark(nx, ny, shark.power, shark.cost + 1));
+                    }
+                }
+            }
+        }
+        System.out.println(ans);
+    }
+ 
+    public static class Shark {
+        int x, y, power, cost;
+ 
+        public Shark(int x, int y, int power, int cost) {
+            this.x = x;
+            this.y = y;
+            this.power = power;
+            this.cost = cost;
+        }
+    }
 }
