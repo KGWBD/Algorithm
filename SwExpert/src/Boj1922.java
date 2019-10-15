aca6d15f8dd982a20b2366337aad06f1a13fb4b6
김건우 <kin0984@naver.com>
2019-10-16 01:34:55 +0900

    네트워크 연결(크루스칼)

---
+++ SwExpert/src/Boj1922.java
@@ -0,0 +1,83 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Boj1922 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	static int[] p;
+	static int N,M;
+	static List<Node> list;
+	public static void main(String[] args) throws IOException {
+		N = Integer.parseInt(br.readLine());
+		M = Integer.parseInt(br.readLine());
+		
+		p = new int[N+1];
+		list = new LinkedList<Boj1922.Node>();
+		makeSet();
+		int now,next,cost;
+		for(int i = 0 ; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			now = Integer.parseInt(st.nextToken());
+			next = Integer.parseInt(st.nextToken());
+			cost = Integer.parseInt(st.nextToken());
+			list.add(new Node(now,next,cost));
+		}
+		Collections.sort(list);
+		int totalCost = 0;
+		for(Node n:list) {
+			if(union(n)) {
+				totalCost+= n.cost;
+			}
+		}
+		System.out.println(totalCost);
+	}
+	
+
+	static void makeSet() {
+		for(int i = 1; i<=N;i++) {
+			p[i] = i;
+		}
+	}
+	
+	static int findSet(int x) {
+		if(p[x] == x)return x;
+		int idx = findSet(p[x]);
+		return p[x] = idx;
+	}
+	
+	static boolean union(Node n) {
+		int x = findSet(n.now);
+		int y = findSet(n.next);
+		if(x!=y) {
+			p[x] = y;
+			return true;
+		}
+		return false;
+	}
+	
+	static class Node implements Comparable<Node>{
+		int now,next,cost;
+		Node(int now, int next, int cost){
+			this.now = now;
+			this.next = next;
+			this.cost = cost;
+		}
+		@Override
+		public int compareTo(Node node) {
+			return cost - node.cost;
+		}
+		@Override
+		public String toString() {
+			return "Node [now=" + now + ", next=" + next + ", cost=" + cost + "]";
+		}
+		
+	}
+}
