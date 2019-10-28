e1f68ba72a28bc6c7572b64ed6267449dfd73a53
김건우 <kin0984@naver.com>
2019-10-28 17:42:37 +0900

    디저트카페(SWExpert수정)

--- SwExpert/src/Problem2105.java
+++ SwExpert/src/Problem2105.java
@@ -1,75 +1,76 @@
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Problem2105 {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringTokenizer st;
-	static boolean[][] visited;
-	static Set<Integer> dessert;
 	static int[][] map;
 	static int[] dx = { 1, -1, -1, 1 };
 	static int[] dy = { 1, 1, -1, -1 };
 	static int N;
-	static int startX, startY;
-	static int max;
+	static int ans; 
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws Exception {
 
 		int T = Integer.parseInt(br.readLine());
-//		int T = 1;
-
 		for (int t = 1; t <= T; t++) {
 			N = Integer.parseInt(br.readLine());
+
 			map = new int[N][N];
-			max = -1;
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < N; j++) {
 					map[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					if ((i == 0 && j == 0) || (i == N - 1 && j == 0) || (i == 0 && j == N - 1)
-							|| (i == N - 1 && j == N - 1))
-						continue;
-					else {
-						startY = i;
-						startX = j;
-						dessert = new HashSet<Integer>();
-						dfs(i, j, 0);
+			ans= Integer.MIN_VALUE;
+			for (int i = 0; i < N - 2; i++) {
+				for (int j = 1; j < N - 1; j++) {
+					Set<Integer> set = new HashSet<>();
+					set.add(map[i][j]);
+					dfs(i, j, i, j, 0, set);
+				}
+			}
+			if(ans==Integer.MIN_VALUE)ans = -1;
+			bw.write("#"+t+" ");
+			bw.write(ans + "\n");
+		}
+		bw.close();
+	}
+
+	private static void dfs(int y, int x, int destY, int destX, int dir, Set<Integer> set) {
+		for (int i = 0; i < 2; i++) {
+			if (dir + i < 4) {
+				int ny = y + dy[dir + i];
+				int nx = x + dx[dir + i];
+				
+				if(ny==destY && nx==destX) {
+					int size = set.size();
+					if(ans<size)ans=size;
+					return;
+				}
+				
+				if (isRange(ny, nx)) {
+					if (!set.contains(map[ny][nx])) {
+						set.add(map[ny][nx]);
+						dfs(ny, nx, destY, destX, dir + i, set);
+						set.remove(map[ny][nx]);
 					}
 				}
 			}
-			System.out.format("#%d %d\n", t, max);
-		} // testCase
+		}
+
 	}
 
-	private static void dfs(int y, int x, int dir) {
-		if (dir == 4)return;
-		if(dir==3 && dessert.size()!=0 && startY == y && startX == x) {
-			if (max < dessert.size())
-				max = dessert.size();
-		}
-
-		if (y + dy[dir] < 0 || y + dy[dir] == N || x + dx[dir] < 0 || x + dx[dir] == N) {
-			return;
-		}
-		
-
-		int des = map[y][x];
-		
-		if(!dessert.contains(des)) {
-		dessert.add(des);
-		dfs(y + dy[dir], x + dx[dir], dir);
-		dfs(y + dy[dir], x + dx[dir], dir + 1);
-		dessert.remove(des);
-		}
-		
+	private static boolean isRange(int ny, int nx) {
+		if (ny < 0 || ny == N || nx < 0 || nx == N)
+			return false;
+		return true;
 	}
-
 }
