48c84dc70f0349c328f3b33eb72f87f8d7d692a5
김건우 <kin0984@naver.com>
2019-11-12 19:55:23 +0900

    정식이의 은행업무

--- SwExpert/src/Problem4366.java
+++ SwExpert/src/Problem4366.java
@@ -6,84 +6,61 @@ import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.PriorityQueue;
+import java.util.Set;
 import java.util.StringTokenizer;
 
-public class Solution {
+public class Problem4366 {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringTokenizer st;
-	static int N;
-	static ArrayList<Node>[] list;
-	static int[] dist;
-	static int ans;
-	static final int INF = Integer.MAX_VALUE;
+	static Set<Integer> set;
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		N = Integer.parseInt(br.readLine().trim());
-		list = new ArrayList[N];
-		for(int i = 0; i < N ;i++) {
-			list[i] = new ArrayList<>();
-		}
-		
-		dist = new int[N];
-		int cost;
-		for(int i = 0 ; i < N ;i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j = 0 ; j < N ;j++) {
-				cost = Integer.parseInt(st.nextToken()); 
-				if(cost!=0) {
-					list[i].add(new Node(j,cost));
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= T; t++) {
+			String[] arr = br.readLine().split("");//1010
+			String[] arr2 = br.readLine().split("");//212
+			String temp;
+			String s = "";
+			set = new HashSet<>();
+			for (int i = 0; i < arr.length; i++) {
+				temp = arr[i];
+				if (temp.equals("0"))
+					arr[i] = "1";
+				else
+					arr[i] = "0";
+				s = "";
+				for (int j = 0; j < arr.length; j++) {
+					s += arr[j];  //0010  1110 1000 1011
+				}
+				set.add(Integer.parseInt(s, 2));
+				arr[i] = temp;
+			}
+			
+			for(int i = 0 ; i < arr2.length; i++) {
+				temp = arr2[i];
+				for(int j = 0 ; j < 3; j++) { //112
+					s="";
+					if(temp.equals(""+j))continue;
+					arr2[i]=""+j;
+					
+					for(int k = 0 ; k < arr2.length;k++) {
+						s+=arr2[k];
+					}
+					
+					if(!set.add(Integer.parseInt(s,3))) {
+						bw.write("#"+t+" ");
+						bw.write(Integer.parseInt(s,3)+"\n");
+					}
 					
 				}
 				
+				
+				arr2[i]=temp;
 			}
 		}
-		
-		Arrays.fill(dist,INF);
-		dijstra(0);
-		System.out.println(Arrays.toString(dist));
-		
-	}
-	
-	
-	
-	private static void dijstra(int start) {
-		dist[start] = 0;
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		pq.add(new Node(start,dist[start]));
-		Node now,next;
-		while(!pq.isEmpty()) {
-			now = pq.poll();
-			for(int i = 0 ; i < list[now.next].size(); i++) {
-				next = list[now.next].get(i);
-				
-				if(dist[next.next] > dist[now.next] + now.cost) {
-					dist[next.next] = dist[now.next] + now.cost;
-					pq.add(new Node(next.next,dist[next.next]));
-				}
-				
-			}
-			
-		}
-		
-	}
-
-
-
-	static class Node implements Comparable<Node>{
-		int next,cost;
-		Node(int next,int cost){
-			this.cost = cost;
-			this.next = next;
-		}
-		@Override
-		public int compareTo(Node o) {
-			return cost - o.cost;
-		}
-		@Override
-		public String toString() {
-			return "Node [next=" + next + ", cost=" + cost + "]";
-		}
-		
+		bw.close();
 	}
 }
