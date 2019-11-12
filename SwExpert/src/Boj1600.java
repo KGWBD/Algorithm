82d5f404a74495d1676b68b2de685dabcb997582
김건우 <kin0984@naver.com>
2019-11-12 19:55:44 +0900

    말이 되고픈 원숭이 메모이제이션

--- SwExpert/src/Boj1600.java
+++ SwExpert/src/Boj1600.java
@@ -6,67 +6,105 @@ import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Set;
 import java.util.StringTokenizer;
 
-public class Main {
+import javax.swing.plaf.synth.SynthSpinnerUI;
+
+public class Boj1600 {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringTokenizer st;
-	static char[][] map;
-	static int R, C;
-	static int[] dx = { 0, 1, 1, 1, 0, -1, -1, -1 };
-	static int[] dy = { -1, -1, 0, 1, 1, 1, 0, -1 };
-
+	static int[][] map;
+	static boolean[][][] visited;
+	static int R, C, K;
+	static int[] dx = {0,0,-1,1,2,1,-1,-2,-2,-1,1,2};
+	static int[] dy = {-1,1,0,0,1,2,2,1,-1,-2,-2,-1};
+	static int ans;
 	public static void main(String[] args) throws IOException {
+		K = Integer.parseInt(br.readLine());
 		st = new StringTokenizer(br.readLine());
-		R = Integer.parseInt(st.nextToken());
+		
 		C = Integer.parseInt(st.nextToken());
-
-		map = new char[R][C];
-
-		for (int i = 0; i < R; i++) {
-			map[i] = br.readLine().toCharArray();
+		R = Integer.parseInt(st.nextToken());
+		
+		map = new int[R][C];
+		visited = new boolean[K+1][R][C];
+		for(int i = 0 ; i < R; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0 ; j < C ; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
 		}
-		int cycle = 0;
-		while (true) {
-			boolean flag = false;
-			for (int i = 0; i < R; i++) {
-				for (int j = 0; j < C; j++) {
-					if (map[i][j] == '.')
-						continue;
-					int cnt = check(i,j);
-					if (map[i][j] - '0' <= cnt) {
-						map[i][j] = '.';
+		
+		bfs(0,0);
+		System.out.println(ans==0?-1:ans);
+		
+	}
+	private static void bfs(int y, int x) {
+		Queue<Position> q = new LinkedList<>();
+		Position pos;
+		int ny,nx;
+		visited[0][y][x] = true;  //
+		q.offer(new Position(y,x,0,0));
+		
+		while(!q.isEmpty()) {
+			pos = q.poll();
+			for(int i = 0 ; i < 12; i++) {
+				ny = pos.y + dy[i];
+				nx = pos.x + dx[i];
+				
+				if(!isRange(ny,nx))continue;
+				if(pos.num>=K && i>=4)break;
+				if(ny==R-1 && nx==C-1) {
+					ans = pos.jump+1;
+					return;
+				}
+				if(i<4) {
+					if(!visited[pos.num][ny][nx] && map[ny][nx]==0) {
+						visited[pos.num][ny][nx] = true;
+						q.offer(new Position(ny,nx,pos.jump+1,pos.num));
+					}
+				} else {
+					if(!visited[pos.num+1][ny][nx] && map[ny][nx]==0) {
+						visited[pos.num+1][ny][nx] = true;
+						q.offer(new Position(ny,nx,pos.jump+1,pos.num+1));
 					}
 				}
-			}
 			
-			printMap();
-			cycle++;
-			if(cycle==2)break;
-
+			}
 		}
+		
+		
+		
 		
 	}
 
-	private static void printMap() {
-		for (int i = 0; i < R; i++) {
-			System.out.println(Arrays.toString(map[i]));
-		}
-		System.out.println("==========================");
 
+
+	private static boolean isRange(int ny, int nx) {
+		if(ny<0 || nx<0|| ny>=R || nx>=C)return false;
+		return true;
 	}
 
-	private static int check(int y, int x) {
-		int cnt = 0;
-		for (int i = 0; i < 8; i++) {
-			if (map[y + dy[i]][x + dx[i]] == '.') {
-				cnt++;
-			}
-		}
-		return cnt;
+	static class Position {
+		int y, x, jump,num;
 
+		public Position(int y, int x, int jump,int num) {
+			this.y = y;
+			this.x = x;
+			this.jump = jump;
+			this.num = num;
+		}
+
+		@Override
+		public String toString() {
+			return "Position [y=" + y + ", x=" + x + ", jump=" + jump + ", num=" + num + "]";
+		}
+
+		
 	}
 
 }
