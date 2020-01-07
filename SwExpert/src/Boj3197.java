343b36292feddd6b14a3f49b671d7029e2e522bd
김건우 <kgw1457@gamil.com>
2020-01-07 22:10:48 +0900

    깃헙으로 이동

--- SwExpert/src/Boj3197.java
+++ SwExpert/src/Boj3197.java
@@ -1,4 +1,154 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-public class Main {
+//맨 처음 사고 -> 1. bfs로 물과 인접한 빙하를 저장하고 나중에 한번에 녹인다.
+//               2. 백조들을 bfs로 만날 수 있는지 확인한다.
+//               3. 만날 수 없으면 1과 2를 반복한다
+//               -> 시간초과 ::  R과 C의 사이즈가 최대 1500이고 매번 반복하기에
+//                              불필요하고 중복적인 부분을 줄여줘야 했다.
+
+//두번째 사고 ->  1. 빙하를 녹일때 모든 물에서 bfs를 시작하지 말고 녹인 빙하부터 녹일
+//                  빙하를 찾아서 저장하고 나중에 한번에 녹인다.
+//               2. 처음사고와 같음
+//               -> 시간초과 :: 매우 조금의 시간을 줄였지 해결책이 아니였다.
+
+
+//세번째 (찬스) -> 1. 처음 사고에서의 1,2번을 계속 반복하는 것이 아니라
+//                bfs 한 번으로 모든 빙하를 녹이고 날짜를 저장해놓는다. -> 불필요한 반복 제거
+//                2. 백조를 만나게 하는데 우선순위큐를 사용해서 가장 적은 날짜를 고른다. 
+
+//알게된 점:: 1. 가중치가 최단거리가 아닌 다른 비용인 경우 우선순위큐를 생각할 수 있어야할 것 같다.
+//           2. 단순하게 한 번 녹이고 찾고 녹이고 찾고 단순반복보다 한 번에 처리하고 다른 
+//           배열에 비용을 저장한 후 찾아내는 방법도 있다는 것을 알게되었다.
+
+public class Boj3197 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	static char[][] map;
+	static int[] dx = { 0, 0, -1, 1 };
+	static int[] dy = { -1, 1, 0, 0 };
+	static int R, C;
+	static int[][] water;
+	static int startY, startX,endY,endX,ans;
+	
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		map = new char[R][C];
+		water = new int[R][C];
+		
+		boolean flag = false;
+		for(int i = 0 ; i < R ; i++) {
+			map[i] = br.readLine().toCharArray();
+			for(int j = 0; j < C ;j++) {
+				if(map[i][j] == 'L') {
+					if(!flag) {
+						startY = i;
+						startX = j;
+						flag = true;
+					} else {
+						endY = i;
+						endX = j;
+					}
+					map[i][j] ='.';
+				}
+			}
+		}//입력
+		
+		melt();
+		meet();
+		System.out.println(ans);
+	}
+	
+	private static void meet() {
+		PriorityQueue<Position> pq = new PriorityQueue<Boj3197.Position>();
+		boolean[][] visited = new boolean[R][C];
+		pq.offer(new Position(startY,startX,0));
+		visited[startY][startX] = true;
+		
+		Position pos;
+		int ny,nx;
+		
+		while(!pq.isEmpty()) {
+			pos = pq.poll();
+			
+			for(int i = 0 ; i < 4; i++) {
+				ny = pos.y + dy[i];
+				nx = pos.x + dx[i];
+				
+				if(!isRange(ny,nx)||visited[ny][nx])continue;
+				
+				if(ny == endY && nx == endX) {
+					ans = pos.day;
+					return;
+				}
+				visited[ny][nx] = true;
+				pq.offer(new Position(ny,nx,Math.max(water[ny][nx], pos.day)));
+			}
+			
+		}
+		
+	}
+	private static void melt() {
+		boolean[][] visited = new boolean[R][C];
+		Queue<Position> q = new LinkedList<Boj3197.Position>();
+		
+		for(int i = 0 ; i < R; i++) {
+			for(int j = 0 ; j < C; j++) {
+				if(map[i][j]=='.') {
+					visited[i][j] = true;
+					q.offer(new Position(i,j,0));
+				}
+			}
+		}
+		
+		Position pos;
+		int ny,nx,size,day = 0;
+		while(!q.isEmpty()) {
+			size = q.size();
+			
+			for(int i = 0 ; i < size; i++) {
+				pos = q.poll();
+				water[pos.y][pos.x] = day;
+				
+				for(int d = 0 ; d < 4 ;d++) {
+					ny = pos.y + dy[d];
+					nx = pos.x + dx[d];
+					if(!isRange(ny,nx) || visited[ny][nx])continue;
+					
+					visited[ny][nx] = true;
+					q.offer(new Position(ny,nx,0));
+				}
+			}
+			day++;
+		}
+	}
+	private static boolean isRange(int ny, int nx) {
+		if(ny<0 || nx<0 || ny==R || nx==C)return false;
+		return true;
+	}
+	static class Position implements Comparable<Position>{
+		int y,x,day;
+		Position(int y,int x,int day){
+			this.y = y;
+			this.x = x;
+			this.day = day;
+		}
+		@Override
+		public int compareTo(Position o) {
+			return this.day - o.day;
+		}
+		
+		
+		
+	}
 }
--- SwExpert/src/Solution.java
+++ SwExpert/src/Solution.java
@@ -1,41 +1,86 @@
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
+import java.util.List;
 
-class Solution {
-  public static int[] solution(int[] arr) {
-      int[] answer = arr;
-      ArrayList<Integer> tempList = new ArrayList<Integer>();
-      
-      for(int i = 0 ; i < answer.length; i++) {
-    	  tempList.add(answer[i]);
-      }
-      Collections.sort(tempList,new Comparator<Integer>() {
-
-		@Override
-		public int compare(Integer o1, Integer o2) {
-			return o2-o1;
+public class Solution {
+	static class File implements Comparable<File>{
+		String file;
+		String head;
+		int number;
+		File(String file){
+			this.file = file;
 		}
-	});
-      
-      tempList.remove(tempList.size()-1);
-      if(tempList.size()==0) {
-    	  answer = new int[] {-1};
-      } else {
-    	  answer = new int[tempList.size()];
-    	  int size = 0;
-    	  for(int i : tempList) {
-    		  answer[size++] = i;
-    	  }
-      }
-      return answer;
-  }
-  public static void main(String[] args) {
-	  int[] arr = {4};
-	  int[] result = solution(arr);
-	  for(int i = 0 ; i<result.length; i++) {
-		  System.out.println(result[i]);
-	  }
-}
-  
+		public void split(int headIdx, int numberIdx) {
+			this.head = file.substring(0, headIdx);
+			if(this.head.contains(" ")) {
+				this.head = this.head.split(" ")[0];
+			}else if(this.head.contains("-")) {
+				this.head = this.head.split("-")[0];
+			}
+			this.number = Integer.parseInt(file.substring(headIdx, numberIdx-1));
+		}
+		@Override
+		public String toString() {
+			return "File [file=" + file + ", head=" + head + ", number=" + number + "]";
+		}
+		@Override
+		public int compareTo(File f) {
+			if(this.head.equalsIgnoreCase(f.head)) {
+				return this.number - f.number;
+			} else {
+				return this.head.compareToIgnoreCase(f.head);
+			}
+		}
+		
+	}
+	public static void main(String[] args) {
+		solution(new String[] {"img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"});
+//		solution(new String[] {"F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"});
+		
+	}
+	
+	public static String[] solution(String[] files) {
+		List<File> fileList = new ArrayList<File>();
+		for(String str : files) {
+			System.out.println(str);
+			int headIdx = 0;
+			int numberIdx = 0;
+			int length = str.length();
+			boolean flag = false;
+			File file = new File(str);
+			for(int i = 0 ; i < length; i++) {
+				char temp = str.charAt(i);
+				if(temp>='0' && temp<='9') {
+					if(!flag) {
+						headIdx =i;
+						flag = true;
+					}
+					numberIdx = i+1;
+					while(numberIdx<length) {
+						temp = str.charAt(numberIdx++);
+						if(temp<'0' || temp>'9') {
+							break;
+						}
+					} // while
+				}
+			}
+			file.split(headIdx, numberIdx);
+			fileList.add(file);
+		}
+		for(File f : fileList) {
+			System.out.println(f);
+		}
+		
+		Collections.sort(fileList);
+
+		System.out.println("-==----------");
+		for(File f : fileList) {
+			System.out.println(f);
+		}
+		String[] answer = new String[fileList.size()];
+		for(int i = 0 ; i < fileList.size(); i++) {
+			answer[i] = fileList.get(i).file;
+		}
+		return answer;
+	}
 }
